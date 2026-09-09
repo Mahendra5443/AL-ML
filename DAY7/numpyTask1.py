e4b9eb719f92bb9cdd488efe1fa6c660dc47c12f
Mahendra5443 <mssisodiya5443@gmail.com>
2026-09-09 14:59:31 +0530

    numpy task done

--- DAY7/numpyTask1.py
+++ DAY7/numpyTask1.py
@@ -14,8 +14,16 @@ a = np.array([
     [1,2,3],
     [4,5,6],
     [7,8,9]
-])
-print(a[1,1]) # print(a[size[0]/2],a[size[1]/2])
+]) # np.arange(1, 10)
+b = np.arange(1, 10)
+b =b.reshape(3,3)
+print(b)
+print(a[1,1]) # or print(a[int(size[0]/2),int(size[1]/2)])
 size = (a.shape)
-print(a[size[0]/2],a[size[1]/2])
+print(a[int(size[0]/2),int(size[1]/2)])
+
 print(size)
+print(a[:,-1])  # print last column
+print(a[-1,:])  # print last raw
+print(np.sum(a,axis=0))  # sum of along raw
+print(np.sum(a,axis=1))  # sum of along column
